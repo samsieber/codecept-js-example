76699df789a7c758745a9b025af60e46c973ce7c
Victor Szoltysek <victorszoltysek@Victors-MacBook-Pro.local>
2016-10-14 15:53:22 -0400

    Add invalid URL for testing purposes.

--- test/example.js
+++ test/example.js
@@ -1,7 +1,7 @@
 Feature('Example.js');
 
 Scenario('search', (I) => {
-  I.amOnPage('http://yahoo.com');
+  I.amOnPage('http://yahoo2.com');
   I.fillField('p', 'github nightmare');
   I.click('Search Web');
   I.waitForElement('#main');
